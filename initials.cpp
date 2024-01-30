b60564929fb5aabb7eb6902d7e9ae274cfb9c7fd
Gregory Szaro <gregoryszaro@gmail.com>
2024-01-30 11:28:05 -0500

    compiled file and coded initials

---
+++ initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  std::cout<<" GGG   SSS \n";
+  std::cout<<"G   G S   S\n";
+  std::cout<<"G     S    \n";
+  std::cout<<"GGGGG   SSS \n";
+  std::cout<<"G   G     S\n";
+  std::cout<<"G   G S   S\n";
+  std::cout<<" GGG   SSS \n";
+
+
+}
